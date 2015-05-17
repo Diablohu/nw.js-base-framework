c99bb7aa8a535d9355920114d364f404807892c5
Diablohu <diablohudream@gmail.com>
2015-05-17 12:29:06 +0800

    Optimize table.js

--- source/js/elements/table.js
+++ source/js/elements/table.js
@@ -6,6 +6,7 @@ _p.el.table = {
 	// is_init: false,
 
 
+	/*
 	hover_column_getTable: function( td ){
 		function _check( el ){
 			if( el[0].tagName.toLowerCase() == 'table' )
@@ -23,6 +24,24 @@ _p.el.table = {
 			return _check( el.parent() )
 		}
 		return _check( td.parent() )
+	},*/
+	hover_column_getTable: function( path ){
+		function _check( index ){
+			if( path[index].tagName.toLowerCase() == 'table' )
+				return $(path[index])
+			
+			return _check( index + 1 )
+		}
+		return _check( 0 )
+	},
+	hover_column_getTr: function( path ){
+		function _check( index ){
+			if( path[index].tagName.toLowerCase() == 'tr' )
+				return $(path[index])
+			
+			return _check( index + 1 )
+		}
+		return _check( 0 )
 	},
 	hover_column_mouseenter: function( table, td_index ){
 		table//.attr( 'td-nth-hovered', parseInt(td_index) + 1 )
@@ -56,6 +75,7 @@ _p.el.table = {
 		els = els || tar.find('table')
 
 		if( !_p.el.table.is_init ){
+			/*
 			$html.on('mouseenter.tablehover-column', 'body.hover table.hashover-column tbody td', function(){
 				//$(this).addClass('state-hover-column')
 				var td = $(this).on('mouseleave.tablehover-column', function(){
@@ -67,6 +87,19 @@ _p.el.table = {
 					// index starts from 0
 					,index = $.inArray( td[0], tr.find('td') )
 
+				_p.el.table.hover_column_mouseenter( table, index )
+			})
+			*/
+			$html.on('mouseenter.tablehover-column', 'body.hover table.hashover-column tbody td', function(e){
+				var path = e.originalEvent.path
+					,td = $(this).on('mouseleave.tablehover-column', function(){
+							_p.el.table.hover_column_mouseleave( table, index )
+							td.off('mouseleave.tablehover-column')
+						})
+					,table = _p.el.table.hover_column_getTable( path )
+					,tr = _p.el.table.hover_column_getTr( path )
+					// index starts from 0
+					,index = $.inArray( td[0], tr.find('td') )
 				_p.el.table.hover_column_mouseenter( table, index )
 			})
 			_p.el.table.is_init = true
