6fe673a81a4422910b4bb1d04f69263551103895
Diablohu <diablohudream@gmail.com>
2015-07-07 12:18:39 +0800

    rename folders

--- source/js-base.js
+++ source/js-base.js
@@ -1,36 +1,40 @@
-// @koala-prepend "js/libs/jquery-2.1.3.min.js"
+/*
+ my base framework for nw.js project: https://github.com/Diablohu/nw.js-base-framework
+ */
 
-// @koala-prepend "js/_g.js"
-// @koala-prepend "js/_p.js"
+// @koala-prepend "js-base/libs/jquery-2.1.3.min.js"
 
-// @koala-prepend "js/libs/visibility/visibility.core.js"
-// @koala-prepend "js/libs/jquery.mousewheel.js"
+// @koala-prepend "js-base/_g.js"
+// @koala-prepend "js-base/_p.js"
 
-// @koala-prepend "js/prototype/date.js"
-// @koala-prepend "js/prototype/object.js"
-// @koala-prepend "js/prototype/string.js"
+// @koala-prepend "js-base/libs/visibility/visibility.core.js"
+// @koala-prepend "js-base/libs/jquery.mousewheel.js"
 
-// @koala-prepend "js/templates/_.js"
+// @koala-prepend "js-base/prototype/date.js"
+// @koala-prepend "js-base/prototype/object.js"
+// @koala-prepend "js-base/prototype/string.js"
 
-// @koala-prepend "js/hotkey/_.js"
+// @koala-prepend "js-base/templates/_.js"
 
-// @koala-prepend "js/form/_.js"
+// @koala-prepend "js-base/hotkey/_.js"
 
-// @koala-prepend "js/node-webkit/!global.js"
-// @koala-prepend "js/node-webkit/configuration.js"
-// @koala-prepend "js/node-webkit/menu.js"
-// @koala-prepend "js/node-webkit/window.js"
+// @koala-prepend "js-base/form/_.js"
 
-// @koala-prepend "js/frame/_global.js"
-// @koala-prepend "js/frame/menu.js"
-// @koala-prepend "js/frame/modal.js"
-// @koala-prepend "js/frame/tip.js"
+// @koala-prepend "js-base/node-webkit/!global.js"
+// @koala-prepend "js-base/node-webkit/configuration.js"
+// @koala-prepend "js-base/node-webkit/menu.js"
+// @koala-prepend "js-base/node-webkit/window.js"
 
-// @koala-prepend "js/elements/_a.js"
-// @koala-prepend "js/elements/form.js"
-// @koala-prepend "js/elements/flexgrid.js"
-// @koala-prepend "js/elements/input.js"
-// @koala-prepend "js/elements/table.js"
-// @koala-prepend "js/elements/tabview.js"
+// @koala-prepend "js-base/frame/_global.js"
+// @koala-prepend "js-base/frame/menu.js"
+// @koala-prepend "js-base/frame/modal.js"
+// @koala-prepend "js-base/frame/tip.js"
 
-// @koala-prepend "js/!last.js"
+// @koala-prepend "js-base/elements/_a.js"
+// @koala-prepend "js-base/elements/form.js"
+// @koala-prepend "js-base/elements/flexgrid.js"
+// @koala-prepend "js-base/elements/input.js"
+// @koala-prepend "js-base/elements/table.js"
+// @koala-prepend "js-base/elements/tabview.js"
+
+// @koala-prepend "js-base/!last.js"
