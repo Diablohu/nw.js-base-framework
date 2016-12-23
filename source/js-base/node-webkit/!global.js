251c9042d568de2332de5ff3c323035fe82d35a0
Diablohu <diablohudream@gmail.com>
2016-12-23 16:52:48 +0800

    more fix to _g.root

--- source/js-base/node-webkit/!global.js
+++ source/js-base/node-webkit/!global.js
@@ -63,21 +63,34 @@ if( global.launcherOptions ){
 
 
 // _g.root, nw.js 程序运行目录
-	_g.root = node.path.dirname(process.execPath).split(node.path.sep)
-	_g.root = (_g.root[_g.root.length - 1] == 'nwjs' && node.path.basename( process.execPath ) == 'nw.exe')
-					? process.cwd()
-					: node.path.dirname(process.execPath)
-	// 对app根目录再做检查，如果不存在，则指向到缓存目录
-	// 该情况通常发生于使用launcer启动时
+	// _g.root = node.path.dirname(process.execPath).split(node.path.sep)
+	// _g.root = (_g.root[_g.root.length - 1] == 'nwjs' && node.path.basename( process.execPath ) == 'nw.exe')
+	// 				? process.cwd()
+	// 				: node.path.dirname(process.execPath)
+	// // 对app根目录再做检查，如果不存在，则指向到缓存目录
+	// // 该情况通常发生于使用launcer启动时
+	// 	try{
+	// 		var stat = node.fs.lstatSync( node.path.join( _g.root , '/app/main.html' ) )
+	// 	}catch(e){
+	// 		_g.root	= node.path.join( node.gui.App.dataPath, '/Extracted Data/')
+	// 	}
+	_g.root = node.path.dirname(process.execPath)
+	try{
+		node.fs.accessSync(
+			node.path.join(_g.root, 'package.json'),
+			node.fs.F_OK
+		);
+	}catch(e){
+		_g.root = node.path.dirname(process.cwd())
 		try{
-			// var stat = node.fs.lstatSync( node.path.join( _g.root , '/app/main.html' ) )
-        	node.fs.accessSync(
+			node.fs.accessSync(
 				node.path.join(_g.root, 'package.json'),
 				node.fs.F_OK
 			);
 		}catch(e){
 			_g.root	= node.path.join( node.gui.App.dataPath, '/Extracted Data/')
 		}
+	}
 
 
 // _g.execPath
